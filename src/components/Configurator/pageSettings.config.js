073c12d9eaf995d13a58348995d8cc29308fc363
GilaWizel <84503123+GilaWizel@users.noreply.github.com>
2021-07-13 13:39:03 +0300

    ff

--- src/components/Configurator/pageSettings.config.js
+++ src/components/Configurator/pageSettings.config.js
@@ -1,5 +1,5 @@
 
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import { Form } from 'react-bootstrap'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { actionsStore } from '../../redux/actions';
